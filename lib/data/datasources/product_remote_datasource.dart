c745e00c1a51ded7d238ca8fa9bc08920114c47a
Henrique Reolon de Lima <pessoal@henriquereolon.dev>
2026-03-13 18:04:17 -0300

    add tratamento de erro p chamada da api

--- lib/data/datasources/product_remote_datasource.dart
+++ lib/data/datasources/product_remote_datasource.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import '../../core/api_client.dart';
 import '../models/product_model.dart';
+import '../../core/exceptions.dart';
 
 class ProductRemoteDataSource {
   final ApiClient apiClient;
@@ -8,8 +9,18 @@ class ProductRemoteDataSource {
   ProductRemoteDataSource({required this.apiClient});
 
   Future<List<ProductModel>> fetchProducts() async {
-    final response = await apiClient.client.get(Uri.parse('https://fakestoreapi.com/products'));
-    final List<dynamic> jsonList = json.decode(response.body);
-    return jsonList.map((json) => ProductModel.fromJson(json)).toList();
+    try {
+      final response = await apiClient.client.get(Uri.parse('https://fakestoreapi.com/products'));
+      
+      if (response.statusCode == 200) {
+        final List<dynamic> jsonList = json.decode(response.body);
+        return jsonList.map((json) => ProductModel.fromJson(json)).toList();
+      } else {
+        throw ServerException('Erro ao carregar produtos. Código: ${response.statusCode}');
+      }
+    } catch (e) {
+      if (e is ServerException) rethrow;
+      throw ServerException('Falha de comunicação com a API');
+    }
   }
 }
